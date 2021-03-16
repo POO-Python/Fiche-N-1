71a51abce3a9d2060e0be6fbddd12a4ed66be66d
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 14:25:00 +0100

    Ajout des getters x et y

--- Vector.py
+++ Vector.py
@@ -3,6 +3,15 @@ class Vector(object):
     def __init__(self, x, y):
         self.coords = (x, y)
 
+    @property 
+    def x (self):
+        return self.coords[0]
+
+    @property 
+    def y (self):
+        return self.coords[1]
+
+
     def __str__(self):
         return '(' + str(self.coords[0]) + ', ' +  str(self.coords[1]) + ')'
 
