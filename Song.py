3a2ea2c9545a0f779e320e4cdc10c00a094128df
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 12:24:37 +0100

    Affiche la liste des artistes qui ont composés ma chanson

--- Song.py
+++ Song.py
@@ -8,5 +8,9 @@ class Song(object):
     def hassinger(self, name):
         return name in self.singers
 
-
+    def __str__(self):
+        result =  self.title + ' par ' + self.singers[0]
+        for i in range(1, len(self.singers)):
+            result += ', ' + self.singers[i]
+        return result
 
