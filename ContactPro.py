f7997b4f3077bd6eb919ff4c97646b255a3b1f2a
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 16:44:22 +0100

    Ajout des getter et setter de ma classe ContactPro

--- ContactPro.py
+++ ContactPro.py
@@ -1,9 +1,25 @@
 from Contact import Contact
 
 class ContactPro(Contact):
+    #Constructeur
     def __init__(self, fname, lname, phone, occ):
         Contact.__init__(self, fname, lname, phone)
         self.occupation = occ
+
+    #Getter
+    @property
+    def occupation(self):
+        return self.__occupation
+
+    #Setter
+    @occupation.setter
+    def occupation(self, value):
+        self.__occupation = value
+
+    #Méthode
+    def __str__(self):
+        return "{} {:>30}".format(Contact.__str__(self), self.occupation)
+
 
 
 
