9d525fd4da6160519c570e8cdab007a54ca8a662
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 15:35:11 +0100

    Modification de ma classe Voiture

--- Voiture.py
+++ Voiture.py
@@ -1,11 +1,26 @@
 class Voiture(object):
-    def __init__(self, marque, color):
-        self.marque = marque
-        self.color = color
     
+    def __init__(self, marque, couleur):
+        self.__marque = marque
+        self.__couleur = couleur
+    
+
+    @property
+    def marque(self):
+        return self.__marque
+
+    @property
+    def couleur(self):
+        return self.__couleur
+
+    @marque.setter
+    def marque(self, value):
+        self.__marque = value
+
+    @couleur.setter
+    def couleur(self, value):
+        self.__couleur = value
+
+
     def __str__(self):
-        return self.marque + ' ' + self.color
-
-
-
-
+        return self.marque + ' ' + self.couleur
