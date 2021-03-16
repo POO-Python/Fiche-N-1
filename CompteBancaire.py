603e6fb640e13012eac3e885a09b34e29a016df9
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 12:58:30 +0100

    Ajout de la méthode permettant de fusionner mes comptes dans la classe CompteBancaire

--- CompteBancaire.py
+++ CompteBancaire.py
@@ -19,6 +19,12 @@ class CompteBancaire(object):
     def __str__ (self):
         return 'Le solde du compte bancaire ' + str(self.numero) + ' de ' + self.nom + ' est de ' +  str(self.solde) + '€'
 
-
+    def __add__(self, other):
+        if self.nom is other.nom :
+            self = CompteBancaire(self.numero, self.nom, self.solde + other.solde)
+            other = CompteBancaire(other.numero, other.nom, 0)
+            print('Fusion effectuée.')
+        else:
+            print('La fusion de ces comptes est impossible.')
 
 
