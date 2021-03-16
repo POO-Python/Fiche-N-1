0175ddb89fd6a218ec8a88d45da274fad5d482b0
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 10:37:25 +0100

    Ajout de la méthode set_phonenumber

--- Contact.py
+++ Contact.py
@@ -1,10 +1,13 @@
 class Contact(object):
     
-    def __init__(self, firstname, lastname, phonenumber):
+    def __init__(self, firstname, lastname, phonenumber=None):
 
         self.firstname = firstname
         self.lastname = lastname
         self.phonenumber = phonenumber
+
+    def set_phonenumber (self):
+        pass
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Contact import Contact
 
 brice = Contact('Brice', 'Smith', 6942)
+obama = Contact('Barack', 'Obama', 9381)
+lurkin = Contact('Quentin', 'Lurkin')
 
-print(brice.firstname)
+print(lurkin)
