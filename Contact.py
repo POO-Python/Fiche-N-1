eb4dabd0ec86b39bd70559f2d6128911b3e77d66
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 16:38:09 +0100

    Modification de la classe Contact

--- Contact.py
+++ Contact.py
@@ -1,17 +1,41 @@
 class Contact(object):
-    
+
+    #Constructeur    
     def __init__(self, firstname, lastname, phonenumber=None):
         self.firstname = firstname
         self.lastname = lastname
         self.phonenumber = phonenumber
 
-    def set_phonenumber (self, newphonenumber):
-        self.phonenumber = newphonenumber
+    #Getters
 
-    def changename (self, newfirstname=None, newlastname=None):
-        if newfirstname is not None :
-            self.firstname = newfirstname
-        if newlastname is not None:
-            self.lastname = newlastname
+    @property
+    def firstname(self):
+        return self.__firstname
+
+    @property
+    def lastname(self):
+        return self.__lastname
+
+    @property
+    def phonenumber(self):
+        return self.__phonenumber
+
+    #Setters
+    @firstname.setter
+    def firstname(self, value):
+        self.__firstname = value
+
+    @lastname.setter
+    def lastname(self, value):
+        self.__lastname = value
+
+    @phonenumber.setter
+    def phonenumber(self, value):
+        self.__phonenumber = value
+
+    #Méthode
+    def __str__(self):
+        return "{:30} {:30} {:10}".format(self.firstname, self.lastname, self.phonenumber)
+
 
 
