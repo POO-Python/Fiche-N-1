cb3aed3daea68b49335056c33e7194f621e71183
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-16 15:20:49 +0100

    Modification du main et ajout du constrcteur et de la méthode str de ma classe

--- Voiture.py
+++ Voiture.py
@@ -1,5 +1,10 @@
 class Voiture(object):
-    pass
+    def __init__(self, marque, color):
+        self.marque = marque
+        self.color = color
+    
+    def __str__(self):
+        return self.marque + ' ' + self.color
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from Voiture import Voiture
 
 def main():
+
+    v1 = Voiture('Renault', 'bleue')
+    v2 = Voiture('Mercedes', 'noire')
+    print(v1)
+    print(v2)
+    v1.marque = 'Skoda'
+    print(v1)
+
 
 main()
