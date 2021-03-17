70cff7a02112fc14bc3b79702e8a79b424030739
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-17 15:58:49 +0100

    Ajout de la classe VC

---
+++ VC.py
@@ -0,0 +1,14 @@
+from Voiture import Voiture
+from NVC import NVC
+class VC(NVC):
+    
+    def __init__ (self, voiture, conducteur='personne'):
+
+        self.marque = Voiture.marque
+        self.couleur = Voiture.color
+        self.conducteur = NVC.conducteur
+
+
+
+
+
