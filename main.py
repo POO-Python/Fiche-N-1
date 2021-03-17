50a4be8a3056785d2b6b4f2ba9370b461ae48bed
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-03-17 15:03:50 +0100

    Test de la méthode accelerer

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def main():
     print(nvc1)
     nvc1.accelerer(1, 5)
     print(nvc1)
+    nvc1.accelerer(-0.4, 5)
+    print(nvc1)
+    nvc1.accelerer(-3, 2)
+    print(nvc1)
+
+    nvc2 = NVC('Ford', 'verte')
+    print(nvc2)
+    nvc2.accelerer(0.8, 5)
+    print(nvc2)
+
 
 
 main()
